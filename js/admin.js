111c94c3181145816c9f85312d780eaee6bfa42c
IvanC-20 <iejcaceres@gmail.com>
2025-06-02 15:31:20 -0300

    Carga de salones, visual simultáneo tabla home

--- js/admin.js
+++ js/admin.js
@@ -1,96 +1,101 @@
-function validateForm(){
-    let name = document.getElementById("name").value;
-    let address = document.getElementById("address").value;
-    let description = document.getElementById("description").value;
-    let price = document.getElementById("price").value;
+function validateForm() {
+    let nombre = document.getElementById("nombre").value;
+    let direccion = document.getElementById("direccion").value;
+    let descripcion = document.getElementById("descripcion").value;
+    let precio = document.getElementById("precio").value;
+    let imagen = document.getElementById("archivo").value
 
-    if(name == ""){
+    if (nombre == "") {
         alert("Debe ingresar Nombre");
         return false
     }
-    if(address == ""){
+    if (direccion == "") {
         alert("Debe ingresar Dirección");
         return false
     }
-    if(description == ""){
+    if (descripcion == "") {
         alert("Debe ingresar Descripción");
         return false
     }
-    if(price == ""){
+    if (precio == "") {
         alert("Debe ingresar Precio");
         return false
     }
-    else if(price<0){
-        alert("El precio no puede ser menor que cero")
+    else if (precio < 0) {
+        alert("El precio no puede ser menor que cero");
         return false
     }
-    return true
+    if (imagen == "" || imagen == "Selecciona la imagen a cargar...") {
+        alert("Debe subir una imagen");
+        return false
+    }
+
+    return true;
 }
 
-function showData(){
+function showData() {
     let listaSalones;
-    if(localStorage.getItem("listaSalones")== null){
+    if (localStorage.getItem("listaSalones") == null) {
         listaSalones = []
     }
-    else{
+    else {
         listaSalones = JSON.parse(localStorage.getItem("listaSalones"))
     }
 
-    let html="";
-    listaSalones.forEach(function(element,index){
+    let html = "";
+    listaSalones.forEach(function (element, index) {
         html += "<tr>";
-        html += "<td>" + element.name +  "</td>";
-        html += "<td>" + element.address +  "</td>";
-        html += "<td>" + element.description +  "</td>";
-        html += "<td>" + element.price +  "</td>";
-        btDelete = '<button onclick ="deleteData(' + index + ')" class="btn btn-danger">Eliminar</button>';
-        btEdit =  '<button onclick ="updateData(' + index + ')" class="btn btn-warning">Editar</button>';
-        html += '<td>'+ btDelete + btEdit +'</td>'
+        html += "<td>" + element.nombre + "</td>";
+        html += "<td>" + element.direccion + "</td>";
+        html += "<td>" + element.descripcion + "</td>";
+        html += "<td><img" + " style='max-width:100%' height='80px'" + " title=" + element.imagen + " src=" + element.imagen + "></td>";
+        html += "<td>" + element.precio + "</td>";
+        btDelete = '<button onclick ="deleteData(' + index + ')" class="btn btn-danger w-100">Eliminar</button>';
+        btEdit = '<button onclick ="updateData(' + index + ')" class="btn btn-warning mt-1 w-100">Editar</button>';
+        html += '<td>' + btDelete + btEdit + '</td>'
     });
     document.querySelector("#crudTable tbody").innerHTML = html;
 }
 
 document.onload = showData()
 
-function addData(){
-    if(validateForm()==true){
-        var name = document.getElementById("name").value;
-        var address = document.getElementById("address").value;
-        var description = document.getElementById("description").value;
-        var price = document.getElementById("price").value;
-    }
+function addData() {
+    if (validateForm() == true) {
+        var nombre = document.getElementById("nombre").value;
+        var direccion = document.getElementById("direccion").value;
+        var descripcion = document.getElementById("descripcion").value;
+        var precio = document.getElementById("precio").value;
+        var imagen = document.getElementById("archivo").value;
 
-    let listaSalones;
-    if(localStorage.getItem("listaSalones")== null){
-        listaSalones = []
-    }
-    else{
-        listaSalones = JSON.parse(localStorage.getItem("listaSalones"))
-    }
-
-    listaSalones.push(
-        {
-            name: name,
-            address: address,
-            description: description,
-            price: price
+        let listaSalones;
+        if (localStorage.getItem("listaSalones") == null) {
+            listaSalones = []
         }
-    )
-    localStorage.setItem("listaSalones", JSON.stringify(listaSalones));
-    showData()
-    
-    document.getElementById("name").value = "";
-    document.getElementById("address").value = "";
-    document.getElementById("description").value = "";
-    document.getElementById("price").value = "";
+        else {
+            listaSalones = JSON.parse(localStorage.getItem("listaSalones"))
+        }
+
+        listaSalones.push(
+            {
+                nombre: nombre,
+                direccion: direccion,
+                descripcion: descripcion,
+                precio: precio,
+                imagen: imagen
+            });
+        localStorage.setItem("listaSalones", JSON.stringify(listaSalones));
+        showData()
+
+    }
+
 }
 
-function deleteData(index){
+function deleteData(index) {
     let listaSalones;
-    if(localStorage.getItem("listaSalones")== null){
+    if (localStorage.getItem("listaSalones") == null) {
         listaSalones = []
     }
-    else{
+    else {
         listaSalones = JSON.parse(localStorage.getItem("listaSalones"))
     }
     listaSalones.splice(index, 1);
@@ -98,36 +103,38 @@ function deleteData(index){
     showData();
 };
 
-function updateData(index){
+function updateData(index) {
     document.getElementById("Submit").style.display = "none";
     document.getElementById("Update").style.display = "block";
 
     let listaSalones;
-    if(localStorage.getItem("listaSalones")== null){
+    if (localStorage.getItem("listaSalones") == null) {
         listaSalones = []
     }
-    else{
+    else {
         listaSalones = JSON.parse(localStorage.getItem("listaSalones"))
     }
-    
-    document.getElementById("name").value = listaSalones[index].name;
-    document.getElementById("address").value = listaSalones[index].address;
-    document.getElementById("description").value = listaSalones[index].description;
-    document.getElementById("price").value = listaSalones[index].price;
 
-    document.querySelector("#Update").onclick = function(){
-        if(validateForm()==true){
-            listaSalones[index].name = document.getElementById("name").value;
-            listaSalones[index].address = document.getElementById("address").value;
-            listaSalones[index].description = document.getElementById("description").value;
-            listaSalones[index].price = document.getElementById("price").value;
-        } 
+    document.getElementById("nombre").value = listaSalones[index].nombre;
+    document.getElementById("direccion").value = listaSalones[index].direccion;
+    document.getElementById("descripcion").value = listaSalones[index].descripcion;
+    document.getElementById("precio").value = listaSalones[index].precio;
+    document.getElementById("imagen").value = listaSalones[index].archivo;
+
+    document.querySelector("#Update").onclick = function () {
+        if (validateForm() == true) {
+            listaSalones[index].nombre = document.getElementById("nombre").value;
+            listaSalones[index].direccion = document.getElementById("direccion").value;
+            listaSalones[index].descripcion = document.getElementById("descripcion").value;
+            listaSalones[index].precio = document.getElementById("precio").value;
+            listaSalones[index].imagen = document.getElementById("archivo").value;
+        }
         localStorage.setItem("listaSalones", JSON.stringify(listaSalones));
         showData();
-        document.getElementById("name").value = "";
-        document.getElementById("address").value = "";
-        document.getElementById("description").value = "";
-        document.getElementById("price").value = "";
+       /* document.getElementById("nombre").value = "";
+        document.getElementById("direccion").value = "";
+        document.getElementById("descripcion").value = "";
+        document.getElementById("precio").value = "";*/
 
         document.getElementById("Submit").style.display = "block";
         document.getElementById("Update").style.display = "none";
