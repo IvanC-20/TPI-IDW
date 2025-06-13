49a7a36b5a29af394468c1c19ddd82f08e83b70a
IvanC-20 <iejcaceres@gmail.com>
2025-06-13 11:19:28 -0300

    login con datos locales

--- js/login.js
+++ js/login.js
@@ -1,24 +1,28 @@
-const cerrarSesionBtn = document.getElementById('cerrarSesionBtn');
+import {login} from './auth.js';
+
+/*const cerrarSesionBtn = document.getElementById('cerrarSesionBtn');
 function cerrarSesion(event) {
     sessionStorage.clear();
     window.location.href = 'login.html';
 
-}
+}*/
 
 if (sessionStorage.getItem('usuario')) {
     alert("Usuario logueado!!");
     window.location.href = "admin.html";
 }
-document.getElementById('loginForm').addEventListener('submit', function (event) {
+document.getElementById('loginForm').addEventListener('submit', async function (event) {
     event.preventDefault();
 
     const usuario = document.getElementById('usuario').value;
     const pass = document.getElementById('Password1').value;
 
-    if (usuario === 'admin@admin.com' && pass === '1234') {
+    const usuarioValidado = await login(usuario, pass);
+    if (usuarioValidado) {
         sessionStorage.setItem('usuario', usuario);
         alert("Logueo exitoso!!");
         window.location.href = "admin.html";
+        
     } else {
         alert('Usuario o contraseña incorrectos!');
     }
